cd4d8c29f55dd5b7432ee2b97676f32781f01d22
alissontroll <130527501+alissontroll@users.noreply.github.com>
2026-02-22 17:37:42 -0300

    Update player.js

--- netlify/functions/player.js
+++ netlify/functions/player.js
@@ -115,7 +115,7 @@ exports.handler = async (event) => {
     if (!res.ok) {
       return { statusCode: res.status, headers: HEADERS, body: JSON.stringify({ error: data.message || "Erro da API" }) };
     }
-    return { statusCode: 200, headers: HEADERS, body: JSON.stringify({ trophies: data.trophies, name: data.name, tag: data.tag }) };
+    return { statusCode: 200, headers: HEADERS, body: JSON.stringify({ trophies: data.trophies, name: data.name, tag: data.tag, brawlerCount: (data.brawlers || []).length }) };
   } catch(e) {
     return { statusCode: 500, headers: HEADERS, body: JSON.stringify({ error: e.message }) };
   }
